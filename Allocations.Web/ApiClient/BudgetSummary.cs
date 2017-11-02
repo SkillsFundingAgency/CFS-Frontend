ae34f77d03cb6081dd52df07bb66750b7cf37f60
Matt Hammond <matt@talkthinkdo.co.uk>
2017-11-02 12:12:22 +0000

    Add basic connected API example

---
+++ Allocations.Web/ApiClient/BudgetSummary.cs
@@ -0,0 +1,8 @@
+﻿namespace Allocations.Web.ApiClient
+{
+    public class BudgetSummary : ResultSummary
+    {
+        public Reference Budget { get; set; }
+        public FundingPolicySummary[] FundingPolicies { get; set; }
+    }
+}
---
+++ Allocations.Web/ApiClient/Reference.cs
@@ -0,0 +1,8 @@
+﻿namespace Allocations.Web.ApiClient
+{
+    public class Reference
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
