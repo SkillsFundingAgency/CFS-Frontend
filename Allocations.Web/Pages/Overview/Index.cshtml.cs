4e254b8238f45a91f98961e698dc15383f0c6b5f
Matt Hammond <matt@talkthinkdo.co.uk>
2017-11-20 13:23:58 +0000

    Fix folder name

--- Allocations.Web/Pages/Overview/Index.cshtml.cs
+++ Allocations.Web/Pages/Overview/Index.cshtml.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Allocations.Web.ApiClient;
+using Allocations.Web.ApiClient.Models.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Allocations.Web.Pages.Allocations
+namespace Allocations.Web.Pages.Overview
 {
     public class IndexModel : PageModel
     {
