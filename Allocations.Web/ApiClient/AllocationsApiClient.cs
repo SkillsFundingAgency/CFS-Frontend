0812c738bf348362a48f27910e4bbd24d32e902d
Matt Hammond <matt@talkthinkdo.co.uk>
2017-11-21 05:37:16 +0000

    Add basic retrieve providers

--- Allocations.Web/ApiClient/AllocationsApiClient.cs
+++ Allocations.Web/ApiClient/AllocationsApiClient.cs
@@ -58,6 +58,16 @@ namespace Allocations.Web.ApiClient
             return await GetAsync<Budget>($"{_specsPath}/budgets?budgetId={id}");
         }
 
+        public async Task<ApiResponse<ProviderTestResult[]>> GetProviderResults(string budgetId)
+        {
+            return await GetAsync<ProviderTestResult[]>($"{_resultsPath}/providers?budgetId={budgetId}");
+        }
+
+        public async Task<ApiResponse<ProviderTestResult>> GetProviderResult(string budgetId, string providerId)
+        {
+            return await GetAsync<ProviderTestResult>($"{_resultsPath}/providers?budgetId={budgetId}&providerId={providerId}");
+        }
+
         public async Task<HttpStatusCode> PostBudget(Budget budget)
         {
             return await PostAsync($"{_specsPath}/budgets", budget);
--- Allocations.Web/Pages/Providers/Index.cshtml.cs
+++ Allocations.Web/Pages/Providers/Index.cshtml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Allocations.Web.ApiClient;
+using Allocations.Web.ApiClient.Models;
+using Allocations.Web.ApiClient.Models.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +12,20 @@ namespace Allocations.Web.Pages.Providers
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AllocationsApiClient _apiClient;
+        public IList<ProviderTestResult> Providers { get; set; }
 
+        public IndexModel(AllocationsApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            var results = await _apiClient.GetProviderResults(id);
+
+            Providers = results.Content;
+            return Page();
         }
     }
 }
