e220bef2b40c1e739d05be62e8599128e8f60755
Horia Bologan <bologan.horia@gmail.com>
2017-11-21 11:30:07 +0000

    Ace editor improvements

--- Allocations.Web/wwwroot/assets/js/main.js
+++ Allocations.Web/wwwroot/assets/js/main.js
@@ -123,7 +123,10 @@
         var JavaScriptMode = ace.require("ace/mode/vbscript").Mode;
         editor = ace.edit(this);
         editorSession = editor.getSession();
-        editorSession.setMode('ace/mode/vbscript');
+        editorSession.setMode({
+            path: 'ace/mode/vbscript',
+            v: Date.now() //small tweak here to update the mode constantly
+        });
         editorSession.setUseWrapMode(true);
         editor.resize()
         editor.setOptions({
