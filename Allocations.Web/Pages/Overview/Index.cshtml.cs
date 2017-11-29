58102f240978a785571f96314e286edfb1371e00
torjoclucian <torjoclucian@gmail.com>
2017-11-29 06:33:08 -0800

    Rounded test rates to closest integer number

--- Allocations.Web/Pages/Overview/Index.cshtml.cs
+++ Allocations.Web/Pages/Overview/Index.cshtml.cs
@@ -19,6 +19,8 @@ namespace Allocations.Web.Pages.Overview
             _apiClient = apiClient;
         }
 
+       
+
         public async Task<IActionResult> OnGetAsync()
         {
             var results = await _apiClient.GetBudgetResults();
