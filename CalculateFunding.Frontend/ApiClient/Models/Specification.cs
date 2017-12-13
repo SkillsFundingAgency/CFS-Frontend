2794e1b5cfb35df56f87ec03e600c05bc10d203b
Matt Hammond <matt@talkthinkdo.co.uk>
2017-12-13 06:05:13 +0000

    More work on prototype

--- CalculateFunding.Frontend/ApiClient/Models/Specification.cs
+++ CalculateFunding.Frontend/ApiClient/Models/Specification.cs
@@ -1,13 +1,50 @@
-﻿using Newtonsoft.Json;
+﻿using CalculateFunding.Frontend.ApiClient.Models;
+using Newtonsoft.Json;
 
 namespace CalculateFunding.Frontend.ApiClient.Models
 {
-    public class Specification
+    public class Policy
     {
         [JsonProperty("id")]
         public string Id { get; set; }
 
         [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+
+    public class SpecificationSnapshot
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("specification")]
+        public Specification Specification { get; set; }
+
+
+        //[JsonProperty("fundingPolicies")]
+        //public FundingPolicy[] FundingPolicies { get; set; }
+
+        //[JsonProperty("datasetDefinitions")]
+        //public DatasetDefinition[] DatasetDefinitions { get; set; }
+
+    }
+
+
+public class Specification
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("currentSnapshotId")]
+        public string CurrentSnaphotId { get; set; }
+
+    [JsonProperty("name")]
         public string Name { get; set; }
 
         [JsonProperty("academicYear")]
---
+++ CalculateFunding.Frontend/Pages/Specs/Policy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using CalculateFunding.Frontend.ApiClient.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalculateFunding.Frontend.Pages.Specs
+{
+    public class PolicyModel : PageModel
+    {
+
+        public Policy Policy { get; set; }
+    
+        public void OnGet()
+        {
+            Policy = new Policy();
+        }
+    }
+
+
+}
