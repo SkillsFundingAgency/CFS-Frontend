149100171b839d181444b25ef0d6c70bb87c6b52
Adam Bryant <adam.bryant@live.co.uk>
2018-01-02 16:46:54 +0000

    added test projects

---
+++ CalculateFunding.FrontEnd.UnitTests/DummyTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculateFunding.FrontEnd
+{
+    [TestClass]
+    public class DummyTest
+    {
+        [TestMethod]
+        public void Just_Testing()
+        {
+            //Arrange
+            int i = 1;
+
+            //Act
+            int j = i++;
+
+            //Assert
+            i.Should().Be(2);
+        }
+    }
+}
