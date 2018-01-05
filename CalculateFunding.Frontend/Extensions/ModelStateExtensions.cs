0e8e1c40dccd4b2eeaac9a627f8b96e8ff4251b5
Adam Bryant <adam.bryant@live.co.uk>
2018-01-05 16:51:59 +0000

    added validation controls

---
+++ CalculateFunding.Frontend/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Mvc.ModelBinding
+{
+    public static class ModelStateExtensions
+    {
+        public static bool IsPropertyInvalid(this ModelStateDictionary modelState, string propertyName)
+        {
+            var entries = modelState.Where(m => string.Equals(m.Key, propertyName, StringComparison.InvariantCultureIgnoreCase));
+
+            return entries.Where(m => m.Value.Errors.Any()).Any();
+        }
+    }
+}
