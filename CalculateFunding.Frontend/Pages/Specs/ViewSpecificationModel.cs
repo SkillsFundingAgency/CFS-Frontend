588c8d878eb41ae467d9f2a4f62b5546a30d6318
Adam Bryant <adam.bryant@live.co.uk>
2018-01-06 21:02:06 +0000

    adding view spec

---
+++ CalculateFunding.Frontend/Pages/Specs/ViewSpecificationModel.cs
@@ -0,0 +1,19 @@
+﻿using CalculateFunding.Frontend.Interfaces.ApiClient;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculateFunding.Frontend.Pages.Specs
+{
+    public class ViewSpecificationModel : PageModel
+    {
+        private readonly ISpecsApiClient _specsClient;
+
+        public ViewSpecificationModel(ISpecsApiClient specsClient)
+        {
+            _specsClient = specsClient;
+        }
+    }
+}
