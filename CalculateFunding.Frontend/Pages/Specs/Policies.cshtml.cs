547f26053753863cdf1f7800ba6e64b170f78e70
Daniel Symonds-Lloyd <daniel@wandlesolutions.com>
2018-01-11 12:44:56 +0000

    Merged PR 2090: Initial Manage Policy skeleton
    Initial Manage Policy skeleton
    
    Related work items: #35397

---
+++ CalculateFunding.Frontend/Pages/Specs/Policies.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculateFunding.Frontend.ApiClient;
+using CalculateFunding.Frontend.ApiClient.Models;
+using CalculateFunding.Frontend.Helpers;
+using CalculateFunding.Frontend.Interfaces.ApiClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalculateFunding.Frontend.Pages.Specs
+{
+    public class PoliciesModel : PageModel
+    {
+        private readonly ISpecsApiClient _specsClient;
+
+        public Specification Specification { get; set; }
+
+        public PoliciesModel(ISpecsApiClient specsClient)
+        {
+            Guard.ArgumentNotNull(specsClient, nameof(specsClient));
+
+            _specsClient = specsClient;
+
+        }
+        public async Task<IActionResult> OnGet(string specificationId)
+        {
+            ApiResponse<Specification> specificationResponse = await _specsClient.GetSpecification(specificationId);
+
+            if(specificationResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return new NotFoundObjectResult("Specification not found");
+            }
+
+            if(specificationResponse.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                return new StatusCodeResult(500);
+            }
+
+            this.Specification = specificationResponse.Content;
+
+            return Page();
+        }
+    }
+}
