a96b7b19adb29bd08007b81cc56093a6d3c3a8f2
Daniel Symonds-Lloyd <daniel@wandlesolutions.com>
2018-01-15 12:01:31 +0000

    Merged PR 2111: Added Validation Summary Tag Helpers and Links
    Related work items: #35384, #35397

---
+++ CalculateFunding.Frontend/TagHelpers/TagHelperConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace CalculateFunding.Frontend.TagHelpers
+{
+    public static class TagHelperConstants
+    {
+        /// <summary>
+        /// Prefix to add to ID's for Validation Summary Anchor Tags
+        /// </summary>
+        public const string ValidationAnchorPrefix = "field";
+
+        /// <summary>
+        /// Validation Anchor ID Separator for characters with invalid values
+        /// </summary>
+        public const string ValidationAnchorSeparator = "-";
+
+    }
+}
---
+++ CalculateFunding.Frontend/TagHelpers/ValidationAnchorTagHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CalculateFunding.Frontend.Helpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CalculateFunding.Frontend.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = ForAttributeName)]
+    public class ValidationAnchorTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "validationanchor-for";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            Guard.ArgumentNotNull(context, nameof(context));
+            Guard.ArgumentNotNull(output, nameof(output));
+
+            var fullName = NameAndIdProvider.GetFullHtmlFieldName(ViewContext, For.Name);
+            var idString = NameAndIdProvider.CreateSanitizedId(ViewContext, fullName, TagHelperConstants.ValidationAnchorSeparator);
+
+            if (string.IsNullOrWhiteSpace(idString))
+            {
+                throw new InvalidOperationException("Unable to determine form ID string");
+            }
+
+            output.Attributes.Add("name", $"{TagHelperConstants.ValidationAnchorPrefix}-{idString}");
+            output.Attributes.Add("class", "form-anchor");
+        }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        /// <summary>
+        /// An expression to be evaluated against the current model.
+        /// </summary>
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+    }
+}
