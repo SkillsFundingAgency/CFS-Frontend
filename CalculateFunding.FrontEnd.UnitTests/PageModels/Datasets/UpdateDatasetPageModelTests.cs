9539082396dde3621b4940ba7bf0cc6913bf86bf
Daniel Symonds-Lloyd <daniel@wandlesolutions.com>
2018-05-03 16:10:47 +0000

    Merged PR 3209: Added Page Tests for UpdateDataset
    Related work items: #48887

--- CalculateFunding.FrontEnd.UnitTests/PageModels/Datasets/UpdateDatasetPageModelTests.cs
+++ CalculateFunding.FrontEnd.UnitTests/PageModels/Datasets/UpdateDatasetPageModelTests.cs
@@ -11,6 +11,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using CalculateFunding.Frontend.Clients.DatasetsClient.Models;
 using CalculateFunding.Frontend.Clients.CommonModels;
+using CalculateFunding.Frontend.Extensions;
 
 namespace CalculateFunding.Frontend.UnitTests.PageModels.Datasets
 {
@@ -49,6 +50,84 @@ namespace CalculateFunding.Frontend.UnitTests.PageModels.Datasets
             pageModel.DatasetVersion
                 .Should()
                 .BeEquivalentTo(datasetVersionResponse, c => c.WithAutoConversion());
+        }
+
+        [TestMethod]
+        public async Task UpdateDatasetPageModel_OnGetAsync_WhenDatasetVersionResponseIsNull_ThenErrorIsReturned()
+        {
+            // Arrange
+            IDatasetsApiClient datasetsApiClient = CreateDatasetsApiClient();
+
+            UpdateDatasetPageModel pageModel = CreatePageModel(datasetsApiClient);
+
+            string datasetId = "ds1";
+
+            datasetsApiClient
+                .GetCurrentDatasetVersionByDatasetId(datasetId)
+                .Returns((ApiResponse<DatasetVersionResponse>)null);
+
+            // Act
+            IActionResult result = await pageModel.OnGetAsync(datasetId);
+
+            // Assert
+            result
+                .Should()
+                .BeOfType<InternalServerErrorResult>()
+                .Which
+                .Value
+                .Should().Be("Datasets verion API response was null");
+        }
+
+        [TestMethod]
+        public async Task UpdateDatasetPageModel_OnGetAsync_WhenDatasetVersionResponseIsNotOk_ThenErrorIsReturned()
+        {
+            // Arrange
+            IDatasetsApiClient datasetsApiClient = CreateDatasetsApiClient();
+
+            UpdateDatasetPageModel pageModel = CreatePageModel(datasetsApiClient);
+
+            string datasetId = "ds1";
+
+            datasetsApiClient
+                .GetCurrentDatasetVersionByDatasetId(datasetId)
+                .Returns(new ApiResponse<DatasetVersionResponse>(System.Net.HttpStatusCode.InternalServerError, null));
+
+            // Act
+            IActionResult result = await pageModel.OnGetAsync(datasetId);
+
+            // Assert
+            result
+                .Should()
+                .BeOfType<InternalServerErrorResult>()
+                .Which
+                .Value
+                .Should().Be("Datasets version API call was not OK, returned 'InternalServerError'");
+        }
+
+        [TestMethod]
+        public async Task UpdateDatasetPageModel_OnGetAsync_WhenDatasetVersionResponseContentIsNull_ThenErrorIsReturned()
+        {
+            // Arrange
+            IDatasetsApiClient datasetsApiClient = CreateDatasetsApiClient();
+
+            UpdateDatasetPageModel pageModel = CreatePageModel(datasetsApiClient);
+
+            string datasetId = "ds1";
+
+            datasetsApiClient
+                .GetCurrentDatasetVersionByDatasetId(datasetId)
+                .Returns(new ApiResponse<DatasetVersionResponse>(System.Net.HttpStatusCode.OK, null));
+
+            // Act
+            IActionResult result = await pageModel.OnGetAsync(datasetId);
+
+            // Assert
+            result
+                .Should()
+                .BeOfType<InternalServerErrorResult>()
+                .Which
+                .Value
+                .Should().Be("Datasets version API content was null");
         }
 
         private static UpdateDatasetPageModel CreatePageModel(
