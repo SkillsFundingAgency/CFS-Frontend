863eb8a2c4d809b82928fc828b634a49328b31cd
Ravi Thippeswamy <ravi.thippeswamy@education.gov.uk>
2018-06-18 08:34:03 +0100

    removing the create status  flag ref

--- CalculateFunding.Frontend/Pages/Scenarios/EditTestScenario.cshtml.cs
+++ CalculateFunding.Frontend/Pages/Scenarios/EditTestScenario.cshtml.cs
@@ -42,15 +42,11 @@ namespace CalculateFunding.Frontend.Pages.Scenarios
 
         public string SpecificationName { get; set; }
 
-        public bool CreateTestScenarioStatus { get; set; }
-
         public ScenarioEditViewModel EditScenarioViewModel { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(string testScenarioId, bool wasCreateSuccess=false)
+        public async Task<IActionResult> OnGetAsync(string testScenarioId)
         {
             Guard.IsNullOrWhiteSpace(testScenarioId, nameof(testScenarioId));
-
-            CreateTestScenarioStatus = wasCreateSuccess;
 
             TestScenario scenario = await GetCurrentScenario(testScenarioId);
 
