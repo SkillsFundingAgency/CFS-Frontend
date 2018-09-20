ec9f1f45c5ee193ba195402728976d543fb5f9ec
Ravi Thippeswamy <ravi.thippeswamy@education.gov.uk>
2018-09-20 14:07:13 +0100

    Disabled 'Refresh button' and code cleanup

--- CalculateFunding.Frontend/Controllers/SpecificationController.cs
+++ CalculateFunding.Frontend/Controllers/SpecificationController.cs
@@ -9,7 +9,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using CalculateFunding.Frontend.Extensions;
 
 namespace CalculateFunding.Frontend.Controllers
 {
--- CalculateFunding.Frontend/Pages/Approvals/ViewFunding2.cshtml.cs
+++ CalculateFunding.Frontend/Pages/Approvals/ViewFunding2.cshtml.cs
@@ -1,54 +1,11 @@
-﻿using AutoMapper;
-using CalculateFunding.Frontend.Helpers;
-using CalculateFunding.Frontend.Interfaces.ApiClient;
-using CalculateFunding.Frontend.ViewModels.Approvals;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+ 
 
 namespace CalculateFunding.Frontend.Pages.Approvals
 {
     public class ViewFunding2Model : PageModel
     {
-
-        private readonly ISpecsApiClient _specsClient;
-        private readonly ICalculationsApiClient _calcsClient;
-        private readonly IResultsApiClient _resultsClient;
-        private readonly ITestEngineApiClient _testEngineClient;
-        private readonly IMapper _mapper;
-
-        public ViewFunding2Model(ISpecsApiClient specsApiClient,
-             ICalculationsApiClient calcsClient,
-             IResultsApiClient resultsClient,
-             ITestEngineApiClient testEngineClient,
-             IMapper mapper)
-        {
-            Guard.ArgumentNotNull(specsApiClient, nameof(specsApiClient));
-            Guard.ArgumentNotNull(calcsClient, nameof(calcsClient));
-            Guard.ArgumentNotNull(resultsClient, nameof(resultsClient));
-            Guard.ArgumentNotNull(testEngineClient, nameof(testEngineClient));
-            Guard.ArgumentNotNull(mapper, nameof(mapper));
-
-            _specsClient = specsApiClient;
-            _calcsClient = calcsClient;
-            _resultsClient = resultsClient;
-            _testEngineClient = testEngineClient;
-            _mapper = mapper;
-        }
-
-        public IEnumerable<SelectListItem> FundingStreams { get; set; }
-
-        public IEnumerable<SelectListItem> FundingPeriods { get; set; }
-
-        public IEnumerable<SelectListItem> Specifications { get; set; }
-
-        public string SelectedFundingStreamId { get; set; }
-
-        public string SelectedFundingPeriodId { get; set; }
-
-        [BindProperty]
-        public ViewFundingVeiwModel viewFundingVeiwModel{ get; set; }
+       
         public void OnGet()
         {
         }
