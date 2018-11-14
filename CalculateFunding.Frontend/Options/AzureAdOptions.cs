f09e93fdaf6922a7d89d96d0e7a9aafbdcc1a18a
David Gent <david@darkhorsesoftware.co.uk>
2018-11-14 12:31:18 +0000

    Change authentication back to old way as blocked getting this working and need to do a deployment urgently

--- CalculateFunding.Frontend/Options/AzureAdOptions.cs
+++ CalculateFunding.Frontend/Options/AzureAdOptions.cs
@@ -2,6 +2,20 @@
 {
     public class AzureAdOptions
     {
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string Instance { get; set; }
+
+        public string Domain { get; set; }
+
+        public string TenantId { get; set; }
+
+        public string CallbackPath { get; set; }
+
+        public bool IsEnabled { get; set; }
+
         public string Groups { get; set; }
     }
 }
