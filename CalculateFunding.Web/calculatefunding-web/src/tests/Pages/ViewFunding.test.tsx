33db1344df045ece463aaca235a11e6135ccbf14
Damon SERJI <Damon.SERJI@education.gov.uk>
2020-02-18 08:31:03 +0000

    Merged PR 6156: add test coverage for elements on viewfunding
    add test coverage for elements on viewfunding
    
    Related work items: #35501

--- CalculateFunding.Web/calculatefunding-web/src/tests/Pages/ViewFunding.test.tsx
+++ CalculateFunding.Web/calculatefunding-web/src/tests/Pages/ViewFunding.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ViewFundingPage from "../../pages/ViewFunding";
 import {FundingPeriod, FundingStream, Specification} from "../../types/viewFundingTypes";
-import {FacetsEntity, PublishedProviderItems} from "../../types/publishedProvider";
+import {FacetsEntity, ProvidersEntity, PublishedProviderItems} from "../../types/publishedProvider";
 import {EffectiveSpecificationPermission} from "../../types/EffectiveSpecificationPermission";
+import {LoadingStatus} from "../../components/LoadingStatus";
 
 const Adapter = require('enzyme-adapter-react-16');
 const enzyme = require('enzyme');
@@ -20,36 +21,92 @@ const mockChangePageState = jest.fn();
 const mockGetUserPermission = jest.fn();
 const mockGetLatestJobForSpecification = jest.fn();
 const mockReleaseFunding = jest.fn();
+const mockProviders: ProvidersEntity[] = [];
+const mockFacetProviderTypeValues = [{name: "facet provider type value 1", count: 1}, {name: "facet provider type value 2", count: 2}];
+const mockFacetLocalAuthorityValues = [{name: "facet local authority value 1", count: 1}, {name: "facet local authority value 2", count: 2}];
+const mockFacetStatusTypeValues = [{name: "facet status value 1", count: 1}, {name: "facet status value 2", count: 2}];
+const mockFacets: FacetsEntity[] = [
+        {
+            name: "test provider type facet entity",
+            facetValues: mockFacetProviderTypeValues
+        },
+        {
+            name: "test local authority facet entity",
+            facetValues: mockFacetLocalAuthorityValues
+        },
+        {
+            name: "test status facet entity",
+            facetValues: mockFacetStatusTypeValues
+        }
+    ];
+const mockPublishedProviderResults = {
+        canPublish: true,
+        canApprove: true,
+        currentPage: 123,
+        endItemNumber: 123,
+        facets: mockFacetProviderTypeValues,
+        pagerState: {
+            currentPage: 123,
+            displayNumberOfPages: 123,
+            lastPage: 123,
+            nextPage: 123,
+            pages: [123],
+            previousPage: 123
+        },
+        providers: mockProviders,
+        startItemNumber: 123,
+        totalErrorResults: 123,
+        totalResults: 123,
+        filteredFundingAmount: 123,
+        totalFundingAmount: 1474900,
+        totalProvidersToApprove: 123,
+        totalProvidersToPublish: 123,
+    };
+const mockSpecifications = {
+        name: "",
+        id: "ABCD",
+        templateIds: {
+            PSG: ""
+        },
+        publishedResultsRefreshedAt: null,
+        providerVersionId: "",
+        lastCalculationUpdatedAt: "",
+        fundingStreams: [],
+        fundingPeriod: {
+            id: "",
+            name: ""
+        },
+        isSelectedForFunding: false,
+        description: "",
+        approvalStatus: ""
+    };
 
-it('will shallow mount', () => {
-    const wrapper = shallow(<ViewFundingPage getSelectedSpecifications={mockGetSelectedSpecification}
-                                             getAllFundingStreams={mockGetAllFundingStreams}
-                                             getSelectedFundingPeriods={mockGetSelectedFundingPeriods}
-                                             getPublishedProviderResults={mockGetPublishedProviderResults}
-                                             getLatestRefreshDate={mockGetLatestRefreshDate}
-                                             getUserPermissions={mockGetUserPermission}
-                                             getLatestJobForSpecification={mockGetLatestJobForSpecification}
-                                             filterPublishedProviderResults={mockFilterPublishedProviderResults}
-                                             refreshFunding={mockRefreshFunding}
-                                             approveFunding={mockApproveFunding}
-                                             changePageState={mockChangePageState}
-                                             latestRefreshDateResults={""}
-                                             specifications={{} as Specification}
-                                             fundingStreams={[] as FundingStream[]}
-                                             selectedFundingPeriods={[] as FundingPeriod[]}
-                                             specificationSelected={false}
-                                             publishedProviderResults={{} as PublishedProviderItems}
-                                             filterTypes={[] as FacetsEntity[]}
-                                             jobId={""}
-                                             refreshFundingJobId={""}
-                                             approveFundingJobId={""}
-                                             pageState={""}
-                                             effectiveSpecificationPermission={{} as EffectiveSpecificationPermission}
-                                             releaseFunding={mockReleaseFunding}
-                                             releaseFundingJobId={""}
-                                             jobCurrentlyInProgress={""}/>);
-    expect(wrapper.find('div.govuk-width-container'));
-});
+const mockViewFundingPageWithSpecification = <ViewFundingPage getSelectedSpecifications={mockGetSelectedSpecification}
+                                                            getAllFundingStreams={mockGetAllFundingStreams}
+                                                            getSelectedFundingPeriods={mockGetSelectedFundingPeriods}
+                                                            getPublishedProviderResults={mockGetPublishedProviderResults}
+                                                            getLatestRefreshDate={mockGetLatestRefreshDate}
+                                                            getUserPermissions={mockGetUserPermission}
+                                                            getLatestJobForSpecification={mockGetLatestJobForSpecification}
+                                                            filterPublishedProviderResults={mockFilterPublishedProviderResults}
+                                                            refreshFunding={mockRefreshFunding}
+                                                            approveFunding={mockApproveFunding}
+                                                            changePageState={mockChangePageState}
+                                                            latestRefreshDateResults={""}
+                                                            specifications={mockSpecifications}
+                                                            fundingStreams={[] as FundingStream[]}
+                                                            selectedFundingPeriods={[] as FundingPeriod[]}
+                                                            specificationSelected={true}
+                                                            publishedProviderResults={mockPublishedProviderResults}
+                                                            filterTypes={mockFacets}
+                                                            jobId={""}
+                                                            refreshFundingJobId={""}
+                                                            approveFundingJobId={""}
+                                                            pageState={""}
+                                                            effectiveSpecificationPermission={{} as EffectiveSpecificationPermission}
+                                                            releaseFunding={mockReleaseFunding}
+                                                            releaseFundingJobId={""}
+                                                            jobCurrentlyInProgress={""}/>;
 
 it('will show the first screen', () => {
     const wrapper = shallow(<ViewFundingPage getSelectedSpecifications={mockGetSelectedSpecification}
@@ -82,3 +139,125 @@ it('will show the first screen', () => {
 
     expect(wrapper.find('#select-funding-stream')).toBeTruthy();
 });
+
+it('will shallow mount', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find(".govuk-width-container");
+
+    expect(actual.children().find(".govuk-width-container").length).toBe(2);
+});
+
+it('will show permission status given a valid specification is selected', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find("PermissionStatus");
+
+    expect(actual.length).toBe(1);
+});
+
+it('will show loading status given a valid specification is selected', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find("LoadingStatus");
+
+    expect(actual.length).toBe(1);
+});
+
+it('will show warning status given a valid specification, page state not IDLE and no jobs in progress', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find(".govuk-warning-text__text");
+
+    expect(actual.html()).toContain("Only one funding stream can be approved or released at a time.");
+});
+
+it('will show formatted total funding amount with correct value in filter area', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find(".viewfunding-filter").find("FormattedNumber");
+
+    expect(actual.props().value).toBe(mockPublishedProviderResults.totalFundingAmount);
+});
+
+it('will show formatted total funding amount with correct value in table headers', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find(".govuk-table__head").find("FormattedNumber");
+
+    expect(actual.length).toBe(2);
+    expect(actual.at(0).props().value).toBe(mockPublishedProviderResults.totalFundingAmount);
+    expect(actual.at(1).props().value).toBe(mockPublishedProviderResults.totalFundingAmount);
+});
+
+it('will show last refresh date given latestRefreshDateResults information is available', () => {
+
+    const testLatestRefreshDateResults = "1 Jan 2020";
+    let mockViewFundingPage = <ViewFundingPage getSelectedSpecifications={mockGetSelectedSpecification}
+                                                                  getAllFundingStreams={mockGetAllFundingStreams}
+                                                                  getSelectedFundingPeriods={mockGetSelectedFundingPeriods}
+                                                                  getPublishedProviderResults={mockGetPublishedProviderResults}
+                                                                  getLatestRefreshDate={mockGetLatestRefreshDate}
+                                                                  getUserPermissions={mockGetUserPermission}
+                                                                  getLatestJobForSpecification={mockGetLatestJobForSpecification}
+                                                                  filterPublishedProviderResults={mockFilterPublishedProviderResults}
+                                                                  refreshFunding={mockRefreshFunding}
+                                                                  approveFunding={mockApproveFunding}
+                                                                  changePageState={mockChangePageState}
+                                                                  latestRefreshDateResults={testLatestRefreshDateResults}
+                                                                  specifications={mockSpecifications}
+                                                                  fundingStreams={[] as FundingStream[]}
+                                                                  selectedFundingPeriods={[] as FundingPeriod[]}
+                                                                  specificationSelected={true}
+                                                                  publishedProviderResults={mockPublishedProviderResults}
+                                                                  filterTypes={mockFacets}
+                                                                  jobId={""}
+                                                                  refreshFundingJobId={""}
+                                                                  approveFundingJobId={""}
+                                                                  pageState={""}
+                                                                  effectiveSpecificationPermission={{} as EffectiveSpecificationPermission}
+                                                                  releaseFunding={mockReleaseFunding}
+                                                                  releaseFundingJobId={""}
+                                                                  jobCurrentlyInProgress={""}/>;
+
+    const wrapper = shallow(mockViewFundingPage);
+
+    expect(wrapper.text()).toContain(`Last refresh on: ${testLatestRefreshDateResults}`);
+});
+
+it('will show last refresh not available given latestRefreshDateResults information is not available', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    expect(wrapper.text()).toContain("Last refresh on: Not Available");
+});
+
+it('will show provider type filter with correct options', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find("#ProviderType");
+
+    expect(actual.find('option').at(0).text()).toBe("Show all");
+    expect(actual.find('option').at(1).text()).toBe(mockFacetProviderTypeValues[0].name);
+    expect(actual.find('option').at(2).text()).toBe(mockFacetProviderTypeValues[1].name);
+});
+
+it('will show local authority filter with correct options', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find("#LocalAuthority");
+
+    expect(actual.find('option').at(0).text()).toBe("Show all");
+    expect(actual.find('option').at(1).text()).toBe(mockFacetLocalAuthorityValues[0].name);
+    expect(actual.find('option').at(2).text()).toBe(mockFacetLocalAuthorityValues[1].name);
+});
+
+it('will show status filter with correct options', () => {
+    const wrapper = shallow(mockViewFundingPageWithSpecification);
+
+    let actual = wrapper.find("#Status");
+
+    expect(actual.find('option').at(0).text()).toBe("Show all");
+    expect(actual.find('option').at(1).text()).toBe(mockFacetStatusTypeValues[0].name);
+    expect(actual.find('option').at(2).text()).toBe(mockFacetStatusTypeValues[1].name);
+});
+
