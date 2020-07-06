7561357e280cb2395fb4231087bb56ed210647b2
Matt Burrell <Matt.BURRELL@EDUCATION.GOV.UK>
2020-07-06 10:17:38 +0000

    Merged PR 7897: Turn off redux-logger in production
    Turn off redux-logger in production
    
    Related work items: #48621

--- CalculateFunding.Web/calculatefunding-web/src/index.tsx
+++ CalculateFunding.Web/calculatefunding-web/src/index.tsx
@@ -3,12 +3,16 @@ import * as ReactDOM from 'react-dom';
 import { Store, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import AppContainer from './containers/AppContainer';
-import {IStoreState, rootReducer} from "./reducers/rootReducer";
-import {composeWithDevTools} from "redux-devtools-extension";
+import { IStoreState, rootReducer } from "./reducers/rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk, { ThunkMiddleware } from "redux-thunk";
 
-const middleware = [logger, thunk as ThunkMiddleware<IStoreState>];
+let middleware = [thunk as ThunkMiddleware<IStoreState>];
+
+if (process.env.NODE_ENV === 'development') {
+    middleware = [...middleware, logger];
+}
 
 const store: Store<IStoreState> = createStore(
     rootReducer,
@@ -17,7 +21,7 @@ const store: Store<IStoreState> = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <AppContainer  />
+        <AppContainer />
     </Provider>,
     document.getElementById('main-content') as HTMLElement
 );
