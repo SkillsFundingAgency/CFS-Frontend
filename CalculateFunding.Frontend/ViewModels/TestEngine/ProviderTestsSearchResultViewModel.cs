90b67eb56fa51227c12d74b07b86bb575b262f15
TATE, Matthew <Matthew.TATE@education.gov.uk>
2021-01-29 12:10:20 +0000

    Merged PR 10540: Update common packages
    Related work items: #58668

--- CalculateFunding.Frontend/ViewModels/TestEngine/ProviderTestsSearchResultViewModel.cs
+++ CalculateFunding.Frontend/ViewModels/TestEngine/ProviderTestsSearchResultViewModel.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
     using System.Linq;
-    using CalculateFunding.Frontend.Clients.ScenariosClient.Models;
     using CalculateFunding.Frontend.ViewModels.Common;
 
     public class ProviderTestsSearchResultViewModel : SearchResultViewModel
