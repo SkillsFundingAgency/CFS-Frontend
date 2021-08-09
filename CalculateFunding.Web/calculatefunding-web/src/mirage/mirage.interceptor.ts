f9b8ca11ef58406ce867626b51ba2cfe37772300
Pete Moody <pete.moody@education.gov.uk>
2021-08-09 16:58:01 +0000

    Merged PR 12844: Refactored Mirage and Cypress integration
    Refactored Mirage and Cypress integration
    
    Related work items: #72055

---
+++ CalculateFunding.Web/calculatefunding-web/src/mirage/mirage.interceptor.ts
@@ -0,0 +1,29 @@
+﻿import {Response, createServer} from 'miragejs'
+
+type TestWindow = Window & {
+    handleFromCypress: (request: Request) => Promise<any>
+}
+
+export const isTestWindow = (window: Window | TestWindow): window is TestWindow => {
+    return window.hasOwnProperty('Cypress')
+}
+
+export const interceptBrowserRequests = () => {
+    if (isTestWindow(window)) {
+        const testWindow: TestWindow = window
+        if (testWindow.handleFromCypress) {
+            createServer({
+                environment: 'test',
+                routes() {
+                    const methods = ['get', 'put', 'patch', 'post', 'delete']
+                    methods.forEach((method) => {
+                        (this as any)[method]('/api/*', async (schema: any, request: Request) => {
+                            const [status, headers, body] = await testWindow.handleFromCypress(request)
+                            return new Response(status, headers, body);
+                        })
+                    })
+                },
+            })
+        }
+    }
+}
