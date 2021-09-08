ef97431d800b8502aa8fef0356e792ebc59a14c8
Matt Burrell <Matt.BURRELL@EDUCATION.GOV.UK>
2021-09-08 14:14:38 +0000

    Merged PR 13216: Add get all channels endpoint
    Add get all channels endpoint
    
    Related work items: #73272

---
+++ CalculateFunding.FrontEnd.UnitTests/Controllers/ChannelControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CalculateFunding.Common.ApiClient.Models;
+using CalculateFunding.Common.ApiClient.Publishing;
+using CalculateFunding.Common.ApiClient.Publishing.Models;
+using CalculateFunding.Frontend.Controllers;
+using CalculateFunding.Frontend.Extensions;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace CalculateFunding.Frontend.UnitTests.Controllers
+{
+    [TestClass]
+    public class ChannelControllerTests
+    {
+        private readonly IPublishingApiClient _publishingApiClient = Substitute.For<IPublishingApiClient>();
+
+        private ChannelController _channelController;
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            _channelController = new ChannelController(_publishingApiClient);
+        }
+
+        [TestMethod]
+        public async Task GetAllChannels_Returns_OkObjectResult()
+        {
+	        IEnumerable<Channel> expectedList = new List<Channel>().AsEnumerable();
+            _publishingApiClient.GetAllChannels()
+                .Returns(new ApiResponse<IEnumerable<Channel>>(HttpStatusCode.OK, expectedList)); ;
+
+            IActionResult actual = await _channelController.GetAllChannels();
+
+            actual.Should().BeOfType<OkObjectResult>();
+        }
+
+        [TestMethod]
+        public async Task GetAllChannels_HandlesReturnedInternalServerError()
+        {
+            string errorMessage = "An error";
+            _publishingApiClient.GetAllChannels()
+                .Returns(new ApiResponse<IEnumerable<Channel>>(HttpStatusCode.InternalServerError, message: errorMessage)); ;
+
+            IActionResult actual = await _channelController.GetAllChannels();
+
+            actual.Should().BeOfType<InternalServerErrorResult>();
+
+            InternalServerErrorResult internalServerErrorResult = actual as InternalServerErrorResult;
+            internalServerErrorResult.Value.Should().Be(errorMessage);
+        }
+    }
+}
---
+++ CalculateFunding.Frontend/Controllers/ChannelController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CalculateFunding.Common.ApiClient.Models;
+using CalculateFunding.Common.ApiClient.Publishing;
+using CalculateFunding.Common.ApiClient.Publishing.Models;
+using CalculateFunding.Common.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculateFunding.Frontend.Controllers
+{
+    [ApiController]
+    public class ChannelController : ControllerBase
+    {
+        private readonly IPublishingApiClient _publishingApiClient;
+
+        public ChannelController(IPublishingApiClient publishingApiClient)
+        {
+            Guard.ArgumentNotNull(publishingApiClient, nameof(publishingApiClient));
+
+            _publishingApiClient = publishingApiClient;
+        }
+
+        [Route("api/releasemanagement/channels")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllChannels()
+        {
+            ApiResponse<IEnumerable<Channel>> response = await _publishingApiClient.GetAllChannels();
+
+            return response.Handle(nameof(IEnumerable<Channel>),
+                onSuccess: x => Ok(x.Content));
+        }
+    }
+}
