ef1db17b9c49b28bac6d548e1cf9fd9b9da1c5fb
Dan Symonds-Lloyd <Daniel.SYMONDS-LLOYD@education.gov.uk>
2022-01-31 12:39:32 +0000

    Merged PR 15093: Calculation results display doesn't support enum types
    Calculation results display doesn't support enum types
    
    Related work items: #86444

---
+++ CalculateFunding.FrontEnd.UnitTests/Extensions/HtmlHelperExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿using CalculateFunding.Frontend.Helpers;
+using CalculateFunding.Frontend.ViewModels.Calculations;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateFunding.Frontend.UnitTests.Extensions
+{
+    [TestClass]
+    public class HtmlHelperExtensionTests
+    {
+        [DataTestMethod]
+        [DataRow(null, CalculationValueTypeViewModel.String, "Excluded")]
+        [DataRow("Hello", CalculationValueTypeViewModel.String, "Hello")]
+        [DataRow(null, CalculationValueTypeViewModel.Number, "Excluded")]
+        [DataRow(435, CalculationValueTypeViewModel.Number, "435")]
+        [DataRow(435.72, CalculationValueTypeViewModel.Number, "435.72")]
+        [DataRow(null, CalculationValueTypeViewModel.Percentage, "Excluded")]
+        [DataRow(50, CalculationValueTypeViewModel.Percentage, "50%")]
+        [DataRow(null, CalculationValueTypeViewModel.Currency, "Excluded")]
+        [DataRow(50, CalculationValueTypeViewModel.Currency, "£50")]
+        [DataRow(50.77, CalculationValueTypeViewModel.Currency, "£50.77")]
+        [DataRow(true, CalculationValueTypeViewModel.Boolean, "True")]
+        [DataRow(false, CalculationValueTypeViewModel.Boolean, "False")]
+        [DataRow(null, CalculationValueTypeViewModel.Boolean, "Excluded")]
+        public void WhenCalcValueIsFormatted_ThenCorrectOutputGiven(object value, CalculationValueTypeViewModel calculationType, string expectedOutput )
+        {
+
+            string result = HtmlHelperExtensions.AsFormatCalculationType(value, calculationType);
+
+            result.Should().Be(expectedOutput);
+        }
+    }
+}
--- CalculateFunding.Frontend/Helpers/HtmlHelperExtensions.cs
+++ CalculateFunding.Frontend/Helpers/HtmlHelperExtensions.cs
@@ -7,27 +7,46 @@ namespace CalculateFunding.Frontend.Helpers
     {
         public static string AsFormatCalculationType(this object value, CalculationValueTypeViewModel calculationValueTypeViewModel)
         {
-	        if (value != null)
-	        {
-		        if (decimal.TryParse(value.ToString(), out decimal decimalValue))
-	            {
-		            switch (calculationValueTypeViewModel)
-		            {
-			            case CalculationValueTypeViewModel.Number:
-				            return decimalValue.AsFormattedNumber();
-			            case CalculationValueTypeViewModel.Percentage:
-				            return decimalValue.AsFormattedPercentage();
-			            case CalculationValueTypeViewModel.Currency:
-				            return decimalValue.AsFormattedMoney();
-			            default:
-				            throw new InvalidOperationException("Unknown calculation type");
-		            }
-	            }
+            if (value == null)
+            {
+                return Properties.PageText.ExcludedText;
 
-		        return value.ToString();
             }
 
-	        return Properties.PageText.ExcludedText;
+            if (calculationValueTypeViewModel == CalculationValueTypeViewModel.String)
+            {
+                return value?.ToString();
+            }
+
+            if (calculationValueTypeViewModel == CalculationValueTypeViewModel.Number ||
+                calculationValueTypeViewModel == CalculationValueTypeViewModel.Percentage ||
+                calculationValueTypeViewModel == CalculationValueTypeViewModel.Currency)
+            {
+                if (value != null)
+                {
+                    if (decimal.TryParse(value.ToString(), out decimal decimalValue))
+                    {
+                        switch (calculationValueTypeViewModel)
+                        {
+                            case CalculationValueTypeViewModel.Number:
+                                return decimalValue.AsFormattedNumber();
+                            case CalculationValueTypeViewModel.Percentage:
+                                return decimalValue.AsFormattedPercentage();
+                            case CalculationValueTypeViewModel.Currency:
+                                return decimalValue.AsFormattedMoney();
+                            default:
+                                throw new InvalidOperationException("Unknown calculation type");
+                        }
+                    }
+                }
+            }
+
+            if (calculationValueTypeViewModel == CalculationValueTypeViewModel.Boolean || value != null)
+            {
+                return value.ToString();
+            }
+
+            throw new InvalidOperationException("Unknown calculation type from CalculationValueTypeViewModel");
         }
     }
 }
