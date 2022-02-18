b7053671ff9188740a3c82f17e04bd3e890470bb
John Bull <john.bull@education.gov.uk>
2022-02-18 10:44:59 +0000

    Merged PR 15425: Updated copy for Confirm Skills page
    Updated copy for Confirm Skills page
    
    Related work items: #86951

--- CalculateFunding.Web/calculatefunding-web/src/pages/ConfirmSkills.tsx
+++ CalculateFunding.Web/calculatefunding-web/src/pages/ConfirmSkills.tsx
@@ -41,44 +41,18 @@ export const ConfirmSkills = () => {
           </div>
           <div className="govuk-grid-row">
             <div className="govuk-grid-column-full">
-              <p className="govuk-body">You must have skills in Visual Basic (VB).</p>
-              <p className="govuk-body">
-                You must know the general principles on how the Education and Skills Funding Agency allocates
-                funding to providers. This includes schools, academies, apprenticeships and other educational
-                and training establishments.
+              <p className="govuk-body">Before using the Calculate funding service you must know the general principles on how the
+                Education and Skills Funding Agency allocates funding to providers.
               </p>
               <p className="govuk-body">If you have any queries, please contact your line manager.</p>
             </div>
           </div>
           <div className="govuk-grid-row">
             <div className="govuk-grid-column-full">
-              <h3 className="govuk-heading-m">Specifications</h3>
-              <p className="govuk-body">
-                A specification contain the required calculations for funding. This includes the required
-                calculations, providers in scope and outputs needed.
-              </p>
-              <p className="govuk-body">
-                A specification is used for a set funding period and one funding streams.
-              </p>
-              <p className="govuk-body">
-                The template calculation links will automatically appear as links for you to populate with the
-                calculation information. These will be displayed to providers on their funding statement.
-                Funding calculations will also be displayed on funding statements.
-              </p>
-            </div>
-          </div>
-          <div className="govuk-grid-row">
-            <div className="govuk-grid-column-full">
               <h3 className="govuk-heading-m">Calculations</h3>
               <p className="govuk-body">
-                You must have a schema set-up in CFS for your specification and have the required data source
-                files uploaded and mapped to work for the calculations. You will use coding language, VB, to
-                create or edit template calculations and additional calculations.
-              </p>
-              <p className="govuk-body">
-                Calculate funding uses ‘Intellisense’ which will help you input functions and links to
-                datasets. This means that you can choose the core provider datasets when creating or editing
-                your specification
+                If you are going to write or approve calculations in CFS you must have skills in Visual Basic (VB). These skills are not
+                required however if you do not propose to write or approve calculations.
               </p>
             </div>
           </div>
@@ -86,42 +60,29 @@ export const ConfirmSkills = () => {
             <div className="govuk-grid-column-full">
               <h3 className="govuk-heading-m">Data Source Files and Datasets</h3>
               <p className="govuk-body">
-                You should only upload a data source file if you are confident that the data it contains, and
-                the data schema used are correct and relevant.
+                You should only upload a data source file if you are confident that the data it contains, and the data schema used are
+                correct and relevant.
               </p>
               <p className="govuk-body">
-                You should only map a data source file to a dataset for a specification if you are confident
-                that the data schema and data source file version are correct.
-              </p>
-              <p className="govuk-body">
-                If you require a specific set of provider data, you can{" "}
-                <a
-                  target="_blank"
-                  className="govuk-link"
-                  href="https://dfe.service-now.com"
-                  rel="noopener noreferrer"
-                >
-                  raise a request from the DfE helpdesk
-                </a>{" "}
-                and it will appear as an option in ‘Core provider data’ option when creating or editing a
-                Specification.
+                You should only map a data source file to a dataset for a specification if you are confident that the data schema and
+                data source file version are correct.
               </p>
             </div>
           </div>
           <div className="govuk-grid-row">
             <div className="govuk-grid-column-full">
               <h3 className="govuk-heading-m">Approving and Releasing Funding</h3>
-              <p className="govuk-body">Only if you have the authority to do so you can approve:</p>
-              <ul className="govuk-list govuk-list--bullet">
-                <li>specifications</li>
-                <li>calculations</li>
-                <li>funding</li>
-              </ul>
               <p className="govuk-body">
-                You may also have the required authority in order to release this funding.
+                You can only approve calculations, specifications and funding in CFS if you have the authority to do so.
+              </p>
+              <p className="govuk-body">
+                You can only release funding from CFS to other services for publishing, contracting and payment if you have the
+                authority to do so.
               </p>
             </div>
           </div>
+
+
           <div className="govuk-grid-row">
             <div className="govuk-grid-column-full">
               <h3 className="govuk-heading-m">Confirmation</h3>
@@ -130,7 +91,7 @@ export const ConfirmSkills = () => {
                 <li>
                   have the required skills, knowledge and experience to use the Calculate funding service
                 </li>
-                <li>will only approve or publish items if you have the authority to do so</li>
+                <li>will only approve or release items if you have the authority to do so</li>
               </ul>
             </div>
           </div>
