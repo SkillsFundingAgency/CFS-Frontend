e62b7ddc243e842b621d29c36acba2d56ac0692a
John Bull <john.bull@education.gov.uk>
2022-06-16 12:33:35 +0000

    Merged PR 17349: Added dynamic link to return
    Added dynamic link to return
    
    Related work items: #96863

--- CalculateFunding.Web/calculatefunding-web/src/pages/Datasets/SqlDataExport/RunExportToSql.tsx
+++ CalculateFunding.Web/calculatefunding-web/src/pages/Datasets/SqlDataExport/RunExportToSql.tsx
@@ -294,7 +294,7 @@ function SuccessMessage({
   const history = useHistory();
 
   function handleContinueClick() {
-    history.push("/");
+    history.push(`/Datasets/Export/RunExportToSql/${specification.id}`);
   }
 
   return (
