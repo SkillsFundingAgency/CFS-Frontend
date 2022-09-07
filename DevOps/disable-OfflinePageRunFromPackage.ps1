86d260d2b4ea3960ba72bd5fad6df58ff91f2289
Matthew ARCHER <Matthew.ARCHER@EDUCATION.GOV.UK>
2022-09-07 12:13:39 +0000

    Merged PR 18466: Add Offline Pages scripts
    Related work items: #95004

---
+++ DevOps/disable-OfflinePageRunFromPackage.ps1
@@ -0,0 +1,31 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $ResourceGroupName,
+    [Parameter(Mandatory=$true)]
+    [string] $WebAppName
+)
+
+# Get web app publishing credentials
+$credentials = az webapp deployment list-publishing-credentials --name $webAppName --resource-group $ResourceGroupName --query '{publishingUserName:publishingUserName, publishingPassword:publishingPassword}'
+$credsObject = $credentials | ConvertFrom-Json
+
+# Create basic auth header
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $credsObject.publishingUserName, $credsObject.publishingPassword)))
+
+#use the “If-Match”=”*” header to disable ETag version check on the server side
+$headers = @{
+    Authorization=("Basic {0}" -f $base64AuthInfo)
+    "If-Match" = "*"
+}
+
+$kuduUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/app_offline.htm"
+
+#Remove Offline Page
+Invoke-RestMethod -Uri $kuduUrl -Headers $headers -Method DELETE -ContentType "multipart/form-data"
+
+#Enable run Website from Package
+az webapp config appsettings set -n $webAppName -g $ResourceGroupName --settings "WEBSITE_RUN_FROM_PACKAGE=1"
+Start-Sleep 5
+
+Write-Host "Offline Page was enabled, now removed"
---
+++ DevOps/enable-OfflinePageRunFromPackage.ps1
@@ -0,0 +1,41 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $ResourceGroupName,
+    [Parameter(Mandatory=$true)]
+    [string] $WebAppName,
+    [Parameter(Mandatory=$true)]
+    [string] $SourcePath
+)
+
+# Ensure file exists
+
+if(-not (Test-Path -Path $SourcePath)) {
+    Write-Error "Could not find file to upload"
+    throw "Could not find file to upload: $SourcePath"
+}
+
+
+# Get web app publishing credentials
+$credentials = az webapp deployment list-publishing-credentials --name $webAppName --resource-group $ResourceGroupName --query '{publishingUserName:publishingUserName, publishingPassword:publishingPassword}'
+$credsObject = $credentials | ConvertFrom-Json
+
+# Create basic auth header
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $credsObject.publishingUserName, $credsObject.publishingPassword)))
+
+#use the “If-Match”=”*” header to disable ETag version check on the server side
+$headers = @{
+    Authorization=("Basic {0}" -f $base64AuthInfo)
+    "If-Match" = "*"
+}
+
+$kuduUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/app_offline.htm"
+
+#Disable run Website from Package
+az webapp config appsettings set -n $webAppName -g $ResourceGroupName --settings "WEBSITE_RUN_FROM_PACKAGE=0"
+Start-Sleep 5
+
+#upload app_offline.htm file
+Invoke-RestMethod -Uri $kuduUrl -Headers $headers -Method PUT -infile $SourcePath -ContentType "multipart/form-data"
+
+Write-Host "Offline Page now enabled"
